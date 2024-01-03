2a0a364f5018b6f1d8be1b3a74c41adf21d86d0d
Ahmadali444 <109669539+Ahmadali444@users.noreply.github.com>
2024-01-03 20:48:08 +0500

    Python

---
+++ Section 2 Start-Course/main.py
@@ -0,0 +1,21 @@
+# Print Name Directly
+print("Ahmed Ali");
+
+# Store Name and Print
+name = "Ahmed Ali";
+print(name);
+
+# Store Name by Taking Input and Print
+name1 = input("Input You Name?");
+print(name1);
+
+# Store Name by Taking Input and Print and Concatenation
+name2 = input("Input You Name?");
+print("My name is: " + name2);
+
+
+
+
+
+
+
